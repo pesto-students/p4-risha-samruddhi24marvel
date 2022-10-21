04aba60b4006c48e3e34e6fdf47e1c52d12c5ccf
samruddhi24marvel <samruddhi241995@gmail.com>
2022-10-22 00:15:15 +0530

    ASPerTechable content

--- Week-9/todo-list/src/App.js
+++ Week-9/todo-list/src/App.js
@@ -1,16 +1,98 @@
-import './App.css';
-import React,  { useState } from 'react';
-import Header from './header';
-import data from "./data.json";
-import ToDoList from "./ToDoList";
+import "./App.css";
+import React, { useState } from "react";
 
-
-function App() {
-  const [ toDoList, setToDoList ]  = useState(data);
+function ToDOInput() {
+  const [value, setValue] = useState("");
+  const [todos, SetTodo] = useState([]);
   return (
-    <div className="App">
-   <Header/>
-   <ToDoList toDoList={toDoList}/>
+    <>
+      <label>
+        Add Your ToDo
+        <input
+          type="text"
+          value={value}
+          onChange={(event) => {
+            const {
+              target: { value },
+            } = event;
+            setValue(value);
+          }}
+        />
+      </label>
+      <div>
+        <button
+          onClick={() => {
+            SetTodo([...todos, { todo: value }]);
+            setValue("");
+          }}
+        >
+          Add toDO
+        </button>
+      </div>
+    </>
+  );
+}
+function DisplayTodo({ todos = [] }) {
+  return (
+    <>
+      <h1>All Todo's</h1>
+      {todos.map((el) => {
+        return <h3>{el.todo}</h3>;
+      })}
+    </>
+  );
+}
+function App() {
+  const [value, setValue] = useState("");
+  const [todos, SetTodo] = useState([]);
+  return (
+    <div class="App">
+      <label>
+        Add Your ToDo
+        <input
+          type="text"
+          value={value}
+          onChange={(event) => {
+            const {
+              target: { value },
+            } = event;
+            setValue(value);
+          }}
+        />
+      </label>
+      <div>
+        <button
+          onClick={() => {
+            SetTodo([...todos, { todo: value, isCompleted: false }]);
+            setValue("");
+          }}
+        >
+          Add toDO
+        </button>
+      </div>
+
+      <h1>All Todo's</h1>
+      {todos.map((el) => {
+        const { isCompleted, todo } = el;
+        return (
+          <>
+            <p style={{ color: isCompleted ? "gren" : "red" }}>{el.todo}</p>
+            <button
+              onClick={() => {
+                const newTodosList = todos.map((el) => {
+                  if (el.todo === todo) {
+                    return { ...el, isCompleted: true };
+                  }
+                  return el;
+                });
+                SetTodo(newTodosList);
+              }}
+            >
+              Done?
+            </button>
+          </>
+        );
+      })}
     </div>
   );
 }
